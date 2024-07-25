f2edf4546c426b7bbabca58290a891e955c359f4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-07-25 18:48:43 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,15 +10,10 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
     triangle = []
-    
     for row in range(n):
         current_row = [1] * (row + 1)
-        
         for j in range(1, row):
             current_row[j] = triangle[row-1][j-1] + triangle[row-1][j]
-        
         triangle.append(current_row)
-    
     return triangle
