9ce9b66c82d5f942768a7163d8a95cc8ec192732
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-01 21:40:45 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Solves the lock boxes puzzle """
 
-
 def look_next_opened_box(opened_boxes):
     """Looks for the next opened box
     Args:
@@ -15,7 +14,6 @@ def look_next_opened_box(opened_boxes):
             return box.get('keys')
     return None
 
-
 def canUnlockAll(boxes):
     """Check if all boxes can be opened
     Args:
@@ -28,25 +26,18 @@ def canUnlockAll(boxes):
 
     aux = {}
     while True:
-        if len(aux) == 0:
-            aux[0] = {
-                'status': 'opened',
-                'keys': boxes[0],
-            }
+        if not aux:
+            aux[0] = {'status': 'opened', 'keys': boxes[0]}
         keys = look_next_opened_box(aux)
         if keys:
             for key in keys:
                 try:
-                    if aux.get(key) and aux.get(key).get('status') \
-                       == 'opened/checked':
+                    if aux.get(key) and aux[key]['status'] == 'opened/checked':
                         continue
-                    aux[key] = {
-                        'status': 'opened',
-                        'keys': boxes[key]
-                    }
+                    aux[key] = {'status': 'opened', 'keys': boxes[key]}
                 except (KeyError, IndexError):
                     continue
-        elif 'opened' in [box.get('status') for box in aux.values()]:
+        elif any(box['status'] == 'opened' for box in aux.values()):
             continue
         elif len(aux) == len(boxes):
             break
@@ -55,11 +46,9 @@ def canUnlockAll(boxes):
 
     return len(aux) == len(boxes)
 
-
 def main():
     """Entry point"""
     canUnlockAll([[]])
-
 
 if __name__ == '__main__':
     main()
