79f7f1eb16f6c374d2fd4ea63a5e139f51f98930
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-08 19:16:44 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,13 +6,22 @@ to result in exactly n 'H' characters.
 
 
 def minOperations(n: int) -> int:
-    """calculates  number of operations
-    """
+    """calculates the fewest number of operations
+    needed to result in exactly n H characters
+    in the file"""
     process = 2
     op = 0
-    while n > 1:
-        if n % process == 0:
-            op += process
-            n /= process
+    while True:
+        op_count = 0
+        do {
+            if n % process == 0:
+                op_count += process
+                n //= process
+            else:
+                break
+        } while (True)
+        if op_count == 0:
+            break
+        op += op_count
         process += 1
     return op
