a5ec6ecbe45c3b85ae4266fd2aa1e1cd7bed832b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-21 13:33:59 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,30 +10,30 @@ def validUTF8(data):
     mask_1 = 1 << 7
     mask_2 = 1 << 6
 
-    for i in data:
-        mask_byte = 1 << 7
-        do_continue = False
-
-        do:
-            if number_bytes == 0:
-                while mask_byte & i:
+    i = 0
+    do:
+        if number_bytes == 0:
+            mask_byte = 1 << 7
+            do:
+                if mask_byte & data[i]:
                     number_bytes += 1
-                    mask_byte = mask_byte >> 1
+                mask_byte = mask_byte >> 1
+            while mask_byte
 
-                if number_bytes == 0:
-                    do_continue = True
-                    break
+            if number_bytes == 0:
+                i += 1
+                continue
 
-                if number_bytes == 1 or number_bytes > 4:
-                    return False
+            if number_bytes == 1 or number_bytes > 4:
+                return False
 
-            else:
-                if not (i & mask_1 and not (i & mask_2)):
-                    return False
+        else:
+            if not (data[i] & mask_1 and not (data[i] & mask_2)):
+                return False
 
-            number_bytes -= 1
-
-        while not do_continue
+        number_bytes -= 1
+        i += 1
+    while i < len(data)
 
     if number_bytes == 0:
         return True
