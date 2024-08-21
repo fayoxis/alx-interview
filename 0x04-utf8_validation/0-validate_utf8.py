545d62f19725f31689b15f34b868db40724034a0
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-21 14:09:50 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
-""" UTF-8 Validation """
+""" UTF-8 Validation Utility """
+
 
 def validUTF8(data):
     """
-    Method that determines if a given data set represents a valid
-    UTF-8 encoding.
+    Checks if given list of integers represents valid UTF-8 encoded string.
+    
+    Args:
+        data (list): A list of integers representing bytes.
+        
+    Returns:
+        bool: True if input data valid UTF-8 encoded string, False otherwise.
     """
-    number_bytes = 0
+    num_bytes = 0
     mask_1 = 1 << 7
     mask_2 = 1 << 6
-
     i = 0
-    do:
-        if number_bytes == 0:
-            mask_byte = 1 << 7
-            do:
-                if mask_byte & data[i]:
-                    number_bytes += 1
-                mask_byte = mask_byte >> 1
-            while mask_byte
 
-            if number_bytes == 0:
+    while i < len(data):
+        mask_byte = 1 << 7
+
+        # Determine the number of bytes in the current UTF-8 character
+        if num_bytes == 0:
+            while mask_byte & data[i]:
+                num_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            # Skip ASCII characters
+            if num_bytes == 0:
                 i += 1
                 continue
 
-            if number_bytes == 1 or number_bytes > 4:
+            # Invalid cases according to the UTF-8 definition
+            if num_bytes == 1 or num_bytes > 4:
                 return False
 
+        # Validate the remaining bytes in the current UTF-8 character
         else:
+            # Check if the current byte has the correct format
             if not (data[i] & mask_1 and not (data[i] & mask_2)):
                 return False
 
-        number_bytes -= 1
+        num_bytes -= 1
         i += 1
-    while i < len(data)
 
-    if number_bytes == 0:
+    # All bytes were processed successfully
+    if num_bytes == 0:
         return True
 
+    # Invalid UTF-8 sequence
     return False
