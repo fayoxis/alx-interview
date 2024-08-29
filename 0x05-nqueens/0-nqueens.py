7cf4952933018893b3c54f60b2cadfbe2509a296
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-29 20:32:52 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,37 +4,42 @@ import sys
 
 if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 n = int(sys.argv[1])
 
 def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
-    result = []
-    do:
-        if i < n:
-            for j in range(n):
-                if j not in a and i + j not in b and i - j not in c:
-                    result = result + queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-        else:
-            result.append(a)
-    while False
-    return result
+    positions = []
+    while i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                new_a = a + [j]
+                new_b = b + [i + j]
+                new_c = c + [i - j]
+                positions.extend(queens(n, i + 1, new_a, new_b, new_c))
+        break
+    if i == n:
+        positions.append(a)
+    return positions
 
 def solve(n):
     """ solve """
     k = []
     i = 0
-    for solution in queens(n, 0):
-        for s in solution:
+    solutions = queens(n, 0)
+    while solutions:
+        solution = solutions.pop(0)
+        while solution:
+            s = solution.pop(0)
             k.append([i, s])
             i += 1
         print(k)
