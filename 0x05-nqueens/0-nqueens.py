42aeccebbf2222fd943b6418db406e61d49661f3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-29 20:13:46 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,8 +2,7 @@
 """ N queens """
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
@@ -11,34 +10,46 @@ if not sys.argv[1].isdigit():
     print("N must be a number")
     exit(1)
 
-if int(sys.argv[1]) < 4:
+n = int(sys.argv[1])
+
+if n < 4:
     print("N must be at least 4")
     exit(1)
 
-n = int(sys.argv[1])
-
-
 def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    solutions = []
+    
+    def backtrack():
+        nonlocal i, a, b, c
+        if i == n:
+            solutions.append(a[:])
+        else:
+            j = 0
+            while True:
+                if j not in a and i + j not in b and i - j not in c:
+                    a.append(j)
+                    b.append(i + j)
+                    c.append(i - j)
+                    i += 1
+                    backtrack()
+                    i -= 1
+                    a.pop()
+                    b.pop()
+                    c.pop()
+                j += 1
+                if j >= n:
+                    break
 
+    backtrack()
+    return solutions
 
 def solve(n):
     """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
+    solutions = queens(n)
+    
+    for solution in solutions:
+        k = [[i, s] for i, s in enumerate(solution)]
         print(k)
-        k = []
-        i = 0
-
 
 solve(n)
