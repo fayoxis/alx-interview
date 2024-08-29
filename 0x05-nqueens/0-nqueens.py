5617a48cbd612d04aff3cc2273ba1bdb69a66c36
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-29 20:26:55 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,41 +2,46 @@
 """ N queens """
 import sys
 
-args_valid = False
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-do:
-    if len(sys.argv) == 2:
-        if sys.argv[1].isdigit():
-            n = int(sys.argv[1])
-            if n >= 4:
-                args_valid = True
-            else:
-                print("N must be at least 4")
-        else:
-            print("N must be a number")
-    else:
-        print("Usage: nqueens N")
-while not args_valid
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
 
 def queens(n, i=0, a=[], b=[], c=[]):
     """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+    while True:
+        if i < n:
+            for j in range(n):
+                if j not in a and i + j not in b and i - j not in c:
+                    yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+        else:
+            yield a
+            break
 
 def solve(n):
     """ solve """
+    solutions = queens(n, 0)
     k = []
     i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    while True:
+        try:
+            solution = next(solutions)
+            for s in solution:
+                k.append([i, s])
+                i += 1
+            print(k)
+            k = []
+            i = 0
+        except StopIteration:
+            break
 
 solve(n)
