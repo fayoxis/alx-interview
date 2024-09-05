60672f3518dcc1726aad59a759a9b854e265dc9c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-05 18:12:38 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, body) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
@@ -11,10 +11,10 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, bod
 const exactOrder = (actors, x) => {
   let i = x;
   while (i < actors.length) {
-    request(actors[i], (err, res, body) => {
+    request(actors[i], function (err, res, body) {
       if (err) throw err;
       console.log(JSON.parse(body).name);
     });
     i++;
   }
-};
+}
