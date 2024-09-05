6e2e42ae5a6cf1919becd41013f7777c769f96d6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-05 19:06:09 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,45 +1,27 @@
 #!/usr/bin/node
 
+// Import 'request' module for making HTTP requests
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-const getCharacterNames = (filmId) => {
-  return new Promise((resolve, reject) => {
-    request(`${API_URL}/films/${filmId}/`, (err, _, body) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      
-      const charactersURL = JSON.parse(body).characters;
-      const charactersName = charactersURL.map(url => {
-        return new Promise((resolveChar, rejectChar) => {
-          request(url, (promiseErr, __, charactersReqBody) => {
-            if (promiseErr) {
-              rejectChar(promiseErr);
-            } else {
-              resolveChar(JSON.parse(charactersReqBody).name);
-            }
-          });
-        });
-      });
+// Make request to Star Wars API for specific film (ID from command line)
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  // Parse JSON response and extract 'characters' array
+  const actors = JSON.parse(body).characters;
+  // Start recursive function to fetch and print character names
+  exactOrder(actors, 0);
+});
 
-      Promise.all(charactersName)
-        .then(names => resolve(names))
-        .catch(allErr => reject(allErr));
-    });
+// Recursive function to fetch and print character names in order
+const exactOrder = (actors, x) => {
+  // Base case: if all actors processed, return
+  if (x === actors.length) return;
+  // Request info about current character
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    // Parse JSON response and print character's name
+    console.log(JSON.parse(body).name);
+    // Recursive call to process next character
+    exactOrder(actors, x + 1);
   });
 };
-
-const main = async () => {
-  for (let i = 2; i < process.argv.length; i++) {
-    try {
-      const names = await getCharacterNames(process.argv[i]);
-      console.log(names.join('\n'));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-};
-
-main();
