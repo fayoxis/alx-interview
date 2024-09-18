8dfd5521f43904aa2383420ef0652a1aae468539
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-18 19:58:54 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
 
-""" Contains makeChange function"""
+""" Make Change Algorithm """
 
 
 def makeChange(coins, total):
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
+    Calculates the minimum number of coins needed to make the given total value.
+    
+    Args:
+        coins (list): A list of coin denominations.
+        total (int): The target value to be made with coins.
+        
+    Returns:
+        int: The minimum number of coins needed to make the target value.
+             If the target value cannot be made with the given coins, returns -1.
+             If the target value is 0 or less, returns 0.
     """
     if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
+
     change = 0
-    coins = sorted(coins)[::-1]
-    for coin in coins:
-        while coin <= total:
+    coins = sorted(coins, reverse=True)
+    i = 0
+
+    while total > 0:
+        if i >= len(coins):
+            return -1
+        coin = coins[i]
+        while total >= coin:
             total -= coin
             change += 1
-        if (total == 0):
-            return change
-    return -1
+        i += 1
+
+    return change
