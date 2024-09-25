843bf7f4a6b17846f900d7879e8a1c9e0d5e09c7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-25 09:01:19 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
-"""Island Perimeter Problem
+""" Island Perimeter Problem This program calculates
+the perimeter of an island represented in a 2D grid.
 """
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in grid
-    Args:
-        grid: 2d list of integers containing 0(water) or 1(land)
-    Return:
-        the perimeter of the island
+    """ Calculate the perimeter of the island in the given grid (list):
+    A 2D list of integers, where 0 represents water and 1
+    represents land. int: The perimeter of the island.
     """
 
-    p = 0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
+            if (grid[i][j] == 1):  # If the current cell is land
                 for case in switch(i, j, grid):
-                    if case(0, 0): # top-left corner
-                        p += 2
+                    if case(0, 0):  # Top-left corner case
+                        perimeter += 2
                         break
-                    if case(0, -1): # top edge
-                        p += 1
+                    if case(0, -1):  # Top edge case
+                        perimeter += 1
                         break
-                    if case(-1, 0): # left edge
-                        p += 1
+                    if case(-1, 0):  # Left edge case
+                        perimeter += 1
                         break
-                    if case(-1, -1): # general case
+                    if case(-1, -1):  # General case
+                        # Check if the current cell is surrounded by water on each side
                         if (i <= 0 or grid[i - 1][j] == 0):
-                            p += 1
+                            perimeter += 1
                         if (j <= 0 or grid[i][j - 1] == 0):
-                            p += 1
+                            perimeter += 1
                         if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                            p += 1
+                            perimeter += 1
                         if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                            p += 1
+                            perimeter += 1
                         break
-    return p
+    return perimeter
 
 def switch(i, j, grid):
+    """ Define cases for different positions of the current cell
+    in the grid. dict_values: A set of lambda functions
+    representing different cases.
+    """
     cases = {
-        (0, 0): lambda: i == 0 and j == 0,
-        (0, -1): lambda: i == 0 and j > 0,
-        (-1, 0): lambda: i > 0 and j == 0,
-        (-1, -1): lambda: True
+        (0, 0): lambda: i == 0 and j == 0,  # Top-left corner
+        (0, -1): lambda: i == 0 and j > 0,  # Top edge
+        (-1, 0): lambda: i > 0 and j == 0,  # Left edge
+        (-1, -1): lambda: True  # General case
     }
     return cases.values()
