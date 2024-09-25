1e16cbb0140eec18098df7a407b3a3b5b08e032f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-25 09:08:21 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,15 +13,23 @@ def island_perimeter(grid):
     """
 
     p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (grid[i][j] == 1):
-                if (i <= 0 or grid[i - 1][j] == 0):
-                    p += 1
-                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    p += 1
-                if (j <= 0 or grid[i][j - 1] == 0):
-                    p += 1
-                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    p += 1
+    i, j = 0, 0
+
+    do:
+        if grid[i][j] == 1:
+            if i == 0 or grid[i - 1][j] == 0:
+                p += 1
+            if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                p += 1
+            if j == 0 or grid[i][j - 1] == 0:
+                p += 1
+            if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                p += 1
+
+        j += 1
+        if j >= len(grid[i]):
+            j = 0
+            i += 1
+    while i < len(grid)
+
     return p
