f5c0107f46edd2618e5ed5780f21d27e81290956
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-10-03 11:50:47 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,55 @@
 #!/usr/bin/python3
-"""0. Prime Game - Maria and Ben are playing a game"""
+"""Prime Game playing a game where they take turns
+choosing different numbers from a set of consecutive integers.
+The game ends when there are no more numbers left to choose.
+The player who picks the last remaining prime number wins the game.
+"""
 
-def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
+
+def isWinner(rounds, numbers):
+    """ Determines the winner of the Prime Game.
+    rounds (int): The number of rounds in the game.numbers
+    (list): The list of consecutive integers to choose from. 
+    str: The name of the winner ("Ben" or "Maria"),
+    or None if there is no winner.
     """
-    if x <= 0 or nums is None:
+    while rounds <= 0 or numbers is None:
         return None
-    if x != len(nums):
+    while rounds != len(numbers):
         return None
 
-    ben = 0
-    maria = 0
+    ben_score = 0
+    maria_score = 0
 
-    max_num = max(nums)
-    a = [1] * (max_num + 1)
-    a[0], a[1] = 0, 0
+    max_num = max(numbers)
+    is_prime = [1] * (max_num + 1)
+    is_prime[0], is_prime[1] = 0, 0
 
     i = 2
-    while i < len(a):
-        rm_multiples(a, i)
+    while i < len(is_prime):
+        remove_multiples(is_prime, i)
         i += 1
 
-    for num in nums:
-        if sum(a[:num + 1]) % 2 == 0:
-            ben += 1
+    for num in numbers:
+        if sum(is_prime[:num + 1]) % 2 == 0:
+            ben_score += 1
         else:
-            maria += 1
+            maria_score += 1
 
-    if ben > maria:
+    while ben_score > maria_score:
         return "Ben"
-    if maria > ben:
+    while maria_score > ben_score:
         return "Maria"
     return None
 
-def rm_multiples(ls, x):
-    """removes multiple
-    of primes
+def remove_multiples(prime_list, x):
+    """ Marks the multiples of a prime number as
+    non-prime in the prime_list. prime_list (list): A list
+    representing prime numbers (1 for prime, 0 for non-prime).
+    x (int): The prime number whose multiples
+    need to be marked as non-prime.
     """
     i = x * 2
-    while i < len(ls):
-        ls[i] = 0
+    while i < len(prime_list):
+        prime_list[i] = 0
         i += x
